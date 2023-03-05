92c6251bc9900e7b111bec76953dca46e1d571d9
miftodevasilica47 <103219315+miftodevasilica47@users.noreply.github.com>
2023-03-05 03:26:22 +0200

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-function changeColor() {
-    var img = document.getElementById('football');
-    var hue = Math.floor(Math.random() * 360);
-    img.style.filter = 'hue-rotate(' + hue + 'deg)';
-    jump();
-  }
