7c31df1ea43751179dad9d73df6c0f4feb63c126
miftodevasilica47 <103219315+miftodevasilica47@users.noreply.github.com>
2023-03-07 03:09:27 +0200

    update

--- donload.php
+++ donload.php
@@ -1,1 +1,7 @@
-<?php phpinfo(); ?>
+<?php
+$file_url = 'http://example.com/music.mp3';
+header('Content-Type: application/octet-stream');
+header("Content-Transfer-Encoding: Binary"); 
+header("Content-disposition: attachment; filename=\"" . basename($file_url) . "\""); 
+readfile($file_url);
+?>
